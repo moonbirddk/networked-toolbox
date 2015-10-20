95fb7309312fce5b99eb90f4a52780858400b259
pawciobiel <pawciobiel@gmail.com>
2015-10-20 15:29:55 +0200

    update requirements.txt and base settings

--- netbox/settings/base.py
+++ netbox/settings/base.py
@@ -15,7 +15,7 @@ import os
 
 DJANGO_ENV = os.getenv('DJANGO_ENV', 'development')
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -102,3 +102,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
