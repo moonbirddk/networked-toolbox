e98d44bf54f120205537224ff0b139d6ad782081
pawciobiel <pawciobiel@gmail.com>
2015-10-27 12:00:03 +0100

    fix tool cover_image deletion

--- tools/views.py
+++ tools/views.py
@@ -3,6 +3,7 @@ import logging
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.db import transaction
+from django.core.files.storage import default_storage
 
 from .forms import ToolForm
 from .models import Tool
@@ -38,18 +39,39 @@ def index(request):
 
 @transaction.atomic
 def edit(request, tool_id):
+    # TODO: use celery tasks for storage IO so id doesn't block transaction
     tool = get_object_or_404(Tool, id=tool_id)
 
-    attributes = {'title': tool.title, 'description': tool.description}
+    attributes = {
+        'title': tool.title,
+        'description': tool.description,
+    }
 
-    form = ToolForm(attributes)
+    if tool.cover_image and default_storage.exists(tool.cover_image.path):
+        files = {'cover_image': tool.cover_image}
+    else:
+        files = {}
+    form = ToolForm(attributes, files)
 
     if request.method == 'POST':
         form = ToolForm(request.POST, request.FILES)
         if form.is_valid():
+            if request.POST.get('cover_image-clear'):
+                cover_image = None
+                if default_storage.exists(tool.cover_image.path):
+                    default_storage.delete(tool.cover_image.path)
+            else:
+                if form.cleaned_data['cover_image']:
+                    if tool.cover_image:
+                        if default_storage.exists(tool.cover_image.path):
+                            default_storage.delete(tool.cover_image.path)
+                    cover_image = form.cleaned_data['cover_image']
+                else:
+                    cover_image = tool.cover_image
+
             tool.title = form.cleaned_data['title']
             tool.description = form.cleaned_data['description']
-            tool.cover_image = form.cleaned_data['cover_image']
+            tool.cover_image = cover_image
             tool.save()
             messages.success(request, "You updated a tool")
             return redirect('tools:index')
