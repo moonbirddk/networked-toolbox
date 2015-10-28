989223022042227c9f86ab52d6bf59901693fab6
Viktor Andersen <sphinxominator@gmail.com>
2015-10-28 12:26:56 +0000

    added tool migration

---
+++ tools/migrations/0004_auto_20151028_1220.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import tools.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tools', '0003_auto_20151023_1240'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='tool',
+            name='cover_image',
+            field=models.ImageField(blank=True, null=True, upload_to=tools.models.do_upload_tool),
+        ),
+        migrations.AlterField(
+            model_name='tool',
+            name='title',
+            field=models.CharField(max_length=100),
+        ),
+    ]
