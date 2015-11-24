3f20e2ca89f0d6f5c51aaaad5f6e3cb6b110f18e
pawciobiel <pawciobiel@gmail.com>
2015-11-24 16:32:48 +0100

    add more tests for tools templatetags

--- tools/tests/tests_templatetags.py
+++ tools/tests/tests_templatetags.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.db.models import FileField
 
 from tools.templatetags.toolstags import css_filefield
+from tools.templatetags.partition_slice import partition, partition_horizontal
 
 
 class CssFileFieldTestCase(TestCase):
@@ -29,3 +30,50 @@ class CssFileFieldTestCase(TestCase):
     def test_css_filefield_unknown(self):
         ff = FileField(name="somename.xyz")
         self.assertEqual('fa-file-text-o', css_filefield(ff))
+
+
+class PartitionSliceTestCase(TestCase):
+
+    def test_partition(self):
+        l = range(10)
+
+        expected = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
+        actual = partition(l, 5)
+        self.assertEqual(expected, actual)
+
+        expected = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9]]
+        actual = partition(l, 4)
+        self.assertEqual(expected, actual)
+
+        expected = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+        actual = partition(l, 3)
+        self.assertEqual(expected, actual)
+
+        expected = [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
+        actual = partition(l, 2)
+        self.assertEqual(expected, actual)
+
+        expected = [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
+        actual = partition(l, 1)
+        self.assertEqual(expected, actual)
+
+    def test_partition_horizontal(self):
+        l = range(2)
+        expected = [[0], [1], []]
+        actual = partition_horizontal(l, 3)
+        self.assertEqual(expected, actual)
+
+        l = range(3)
+        expected = [[0], [1], [2]]
+        actual = partition_horizontal(l, 3)
+        self.assertEqual(expected, actual)
+
+        l = range(4)
+        expected = [[0, 3], [1], [2]]
+        actual = partition_horizontal(l, 3)
+        self.assertEqual(expected, actual)
+
+        l = range(1, 11)
+        expected = [[1, 4, 7, 10], [2, 5, 8], [3, 6, 9]]
+        actual = partition_horizontal(l, 3)
+        self.assertEqual(expected, actual)
