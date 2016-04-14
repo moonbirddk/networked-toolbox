92a6ff266ed8d15817c72d73622427675964ef2c
Simon Carstensen <me@simonbc.com>
2016-04-14 15:50:42 +0200

    add folding/unfolding of categories list on each category group to toolbox overview page

--- tools/static/tools/javascript/tools.js
+++ tools/static/tools/javascript/tools.js
@@ -3,3 +3,12 @@ $(document).on("click", "#show-all-stories", function(e){
   $('.story.hidden').removeClass('hidden');
   $('#show-all-stories').hide();
 });
+
+$(document).on("click", ".category-toggle-fold", function(e){
+  e.preventDefault();
+  $(this).find(':first-child').toggleClass('glyphicon-triangle-right');
+  $(this).find(':first-child').toggleClass('glyphicon-triangle-bottom');
+  $(this).closest('.category-group').next().slideToggle();
+
+
+});
