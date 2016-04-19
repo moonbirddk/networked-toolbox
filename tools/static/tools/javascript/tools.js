39fd1424311c83da1bea0ad2e4faeb13540507df
Simon Carstensen <me@simonbc.com>
2016-04-19 11:34:07 +0200

    link categories to category group

--- tools/static/tools/javascript/tools.js
+++ tools/static/tools/javascript/tools.js
@@ -4,11 +4,22 @@ $(document).on("click", "#show-all-stories", function(e){
   $('#show-all-stories').hide();
 });
 
+function toggleCategoryGroup(group) {
+  group.find(':first-child').toggleClass('glyphicon-triangle-right');
+  group.find(':first-child').toggleClass('glyphicon-triangle-bottom');
+  group.closest('.category-group').next().slideToggle();
+}
+
 $(document).on("click", ".category-toggle-fold", function(e){
   e.preventDefault();
-  $(this).find(':first-child').toggleClass('glyphicon-triangle-right');
-  $(this).find(':first-child').toggleClass('glyphicon-triangle-bottom');
-  $(this).closest('.category-group').next().slideToggle();
+  toggleCategoryGroup($(this));
+});
 
-
+$(document).ready(function(){
+  var url = $(location).attr('href');
+  var hash = url.substring(url.indexOf("#")+1);
+  if (hash) {
+    var group = $('#'+hash+'-link');
+    toggleCategoryGroup(group);
+  }
 });
