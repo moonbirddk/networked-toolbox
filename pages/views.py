ff6be847918760d81a0b7cf2fd401e7f35570e0d
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-09-16 20:19:43 +0200

    Created a view to show a page

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Page
 
-# Create your views here.
+def show(request, page_slug):
+    if request.user.has_perm('pages.change_page'):
+        page = get_object_or_404(Page, slug=page_slug)
+    else:
+        topageol = get_object_or_404(Page, slug=page_slug, published=True)
+    context = {
+        'page': page
+    }
+    return render(request, 'pages/show.html', context)
