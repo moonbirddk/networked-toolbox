13c304e994aeb54a24853676cf6fe4104c4c6161
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-09-16 20:04:57 +0200

    Changed the order of fields on the administrative interface

--- pages/admin.py
+++ pages/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Page
 
 class PageAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title", )}
+    prepopulated_fields = {'slug': ('title', )}
+    fields = ('title', 'slug', 'content', )
+
 admin.site.register(Page, PageAdmin)
