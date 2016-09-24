1dbdd5a527dd356145f724525dca3cdfef23f9af
Andreas Larsen <andreas@larsenwork.com>
2016-09-24 12:27:17 +0200

    Added UI for notications and badge. And button to mark all as read (not working yet though)

--- netbox/static/netbox/javascript/notifications.js
+++ netbox/static/netbox/javascript/notifications.js
@@ -1,18 +1,48 @@
 function netbox_fill_notification_list(data) {
   var $list = $('#live_notify_list').empty();
-  if(data.unread_list) {
-    for (var i=0; i < data.unread_list.length; i++) {
-      var notification = data.unread_list[i];
-      var notificationData = JSON.parse(notification.data) || {};
+  if($list) {
+    if(data.unread_list.length == 0) {
+      $('<li class="no-notification">')
+        .wrapInner('You have no unread notifications')
+        .appendTo($list);
+      $('#mark_all_notifications_as_read').hide();
+    } else {
+      $('#mark_all_notifications_as_read').show();
+      for (var i=0; i < data.unread_list.length; i++) {
+        var notification = data.unread_list[i];
+        var notificationData = JSON.parse(notification.data) || {};
+        var { actor, verb, description } = notification
+        var link = notificationData.actions[0].href;
 
-      console.log('notification #' + i + ': ', notification);
-      var $actor = $('<span>').text(notification.actor);
-      var $verb = $('<span>').text(notification.verb);
-      var $description = $('<span>').text(notification.description);
+        actor = '<span class="notification__actor">' + actor + ' </span>';
+        verb = '<span class="notification__verb">' + verb + '</span>';
+        description = '<span class="notification__text">' + description + '</span>';
+        var content = '<div class="no-wrap">' + actor + verb + '</div>' + description;
 
-      console.log('actions = ', notificationData.actions);
+        var classes = 'notification'
 
-      $('<li>').append($actor, $verb, $description).appendTo($list);
+        $('<li class="notification">')
+          .wrapInner('<a href="' + link + '">' + content + '</a>')
+          .appendTo($list);
+      }
     }
   }
+}
+
+function fill_notification_badge(data) {
+  var $badge = $('#live_notify_badge').addClass('notifications__count--zero');
+  if ($badge) {
+    if (data.unread_count > 0) {
+      $badge.removeClass('notifications__count--zero');
+      $badge.html(data.unread_count)
+    }
+    if (data.unread_count > 9) {
+      $badge.html('9+')
+    }
+  }
+}
+
+function mark_all_notifications_as_read() {
+  $('.notification').addClass('notification--read');
+  console.log('Der skal ske noget mere...');
 }
