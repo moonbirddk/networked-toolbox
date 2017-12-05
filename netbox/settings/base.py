a98abec60cc98aff8fb57145157cb616d6f8caee
MathiasWedeken <mathias@moonbird.dk>
2017-12-05 20:09:31 +0100

    updated local settings again.

--- netbox/settings/base.py
+++ netbox/settings/base.py
@@ -189,21 +189,21 @@ WSGI_APPLICATION = 'netbox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
-#     'default':{ 
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'nwt_production',
-#             'USER': 'postgres',
-#             'HOST': 'localhost',
-#             'PORT': '',
-#             }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+DATABASES = {
+    'default':{ 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'nwt_production',
+            'USER': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '',
+            }
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
