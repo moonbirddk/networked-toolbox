c9cae3b7d895d88a5df24f4cbb955eb2c212499c
William Larsson <william@moonbird.dk>
2018-01-25 16:11:36 +0100

    updated workareas overview and icons paths

--- tools/static/tools/javascript/tools.js
+++ tools/static/tools/javascript/tools.js
@@ -1,6 +1,6 @@
 $(document).on("click", "#show-all-stories", function(e){
   e.preventDefault();
-  $('.story.hidden').removeClass('hidden');
+  $('.hidden').removeClass('hidden');
   $('#show-all-stories').hide();
 });
 
