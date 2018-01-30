6ccd609b507a8af38551204cdfe084870edc19a9
William Larsson <william@moonbird.dk>
2018-01-30 15:49:27 +0100

    updated a lot off stuff: profilephoto, cards icons...

--- profiles/templatetags/profilestags.py
+++ profiles/templatetags/profilestags.py
@@ -5,7 +5,22 @@ register = template.Library()
 
 
 @register.inclusion_tag('profiles/_profile_photo.html')
-def render_profile_photo(user, show_link=True, show_name=False):
+def render_profile_photo(user, show_link=True):
+    """
+    {% render_profile_photo user %}
+    """
+    photo_url = get_profile_photo_url(user)
+    return {
+        'photo_url': photo_url,
+        'profile_uid': user.profile.uid,
+        'show_link': show_link,
+        'title': user.profile.name,
+        'user': user
+    }
+
+
+@register.inclusion_tag('profiles/_profile_photo.html')
+def render_profile_photo_with_name(user, show_link=True, show_name=True):
     """
     {% render_profile_photo user %}
     """
