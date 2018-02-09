c910295f40f51f1d6de2c400c48793438e5205fb
MathiasWedeken <mathias@moonbird.dk>
2018-02-09 23:58:58 +0100

    getting there

--- activities/migrations/0008_auto_20180209_2141.py
+++ activities/migrations/0008_auto_20180209_2141.py
@@ -8,8 +8,8 @@ import re
 def link_to_reverse_url(apps, schema_editor): 
     ActivityEntry = apps.get_model('activities', 'ActivityEntry')
     for entry in ActivityEntry.objects.all():
-        cleaned_url = re.sub(r'#comment-\d+','',reverse_url)
-        link = '{}{}'.format(reverse(entry.reverse_url, args=[entry.related_id]), entry.hashtag)
+        cleaned_url = re.sub(r'#comment-\d+','',entry.reverse_url)
+        link = '{}{}'.format(reverse(cleaned_url, args=[entry.related_id]), entry.hashtag)
         entry.reverse_url = link
         entry.save()
 
