d3d120de2377b5ca00245cb37e45aa81036ae649
MathiasWedeken <mathias@moonbird.dk>
2020-09-15 09:39:22 +0200

    fixed profile stuff

--- profiles/templatetags/profilestags.py
+++ profiles/templatetags/profilestags.py
@@ -10,13 +10,15 @@ def render_profile_photo(user, show_link=True):
     {% render_profile_photo user %}
     """
     photo_url = get_profile_photo_url(user)
-    return {
-        'photo_url': photo_url,
-        'profile_uuid': user.profile.uuid,
-        'show_link': show_link,
-        'title': user.profile.name,
-        'user': user
-    }
+    
+    if hasattr(user, "profile"): 
+        return {
+            'photo_url': photo_url,
+            'profile_uuid': user.profile.uuid,
+            'show_link': show_link,
+            'title': user.profile.name,
+            'user': user
+        }
 
 
 @register.inclusion_tag('profiles/_profile_photo.html')
@@ -25,14 +27,16 @@ def render_profile_photo_with_name(user, show_link=True, show_name=True):
     {% render_profile_photo user %}
     """
     photo_url = get_profile_photo_url(user)
-    return {
-        'photo_url': photo_url,
-        'profile_uuid': user.profile.uuid,
-        'show_link': show_link,
-        'show_name': show_name,
-        'title': user.profile.name,
-        'user': user
-        }
+    if hasttr(user, "profile"): 
+        return {
+            'photo_url': photo_url,
+            'profile_uuid': user.profile.uuid,
+            'show_link': show_link,
+            'show_name': show_name,
+            'title': user.profile.name,
+            'user': user
+            }
+    
 
 @register.filter
 def anim_delay(num, ad=0): 
--- profiles/utils.py
+++ profiles/utils.py
@@ -10,8 +10,9 @@ def get_profile_url(user):
 def get_profile_photo_url(user):
     photo_url = settings.STATIC_URL + 'profiles/images/defaultUser.svg'
     photo_url = quote(photo_url)
-    if user and not user.is_anonymous and user.profile and\
-            user.profile.photo and user.profile.photo.url:
-            #user.profile.has_existing_photo():
+    if (user is not None and 
+        not user.is_anonymous and 
+        hasattr(user,"profile") and
+            user.profile.photo is not None and user.profile.photo.url is not None):
         photo_url = user.profile.photo.url
     return photo_url
